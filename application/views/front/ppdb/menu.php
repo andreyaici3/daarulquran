cf94a27a2e3dbc9a59cb4477df7049a3490b263a
andreyaici3 <andreyandri90@gmail.com>
2020-04-04 15:12:53 +0700

    menambahkan menu home di ppdb

--- application/views/front/ppdb/menu.php
+++ application/views/front/ppdb/menu.php
@@ -37,6 +37,9 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="<?= base_url(); ?>">HOME</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#informasi">INFORMASI</a>
           </li>
           <li class="nav-item mx-0 mx-lg-1">
